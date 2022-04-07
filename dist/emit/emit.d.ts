04f1f47ea7fee4182b18ca6d405e78294b90e5d5
dikac <rigel234@yahoo.com>
2022-04-07 15:05:41 +0700

    exclude dist from git

--- dist/emit/emit.d.ts
+++
@@ -1,4 +0,0 @@
-export default interface Emit<Arguments extends any[] = any[], Return = undefined> {
-    readonly emittable: boolean;
-    emit(...Arguments: any[]): Return;
-}
--- dist/listenable/listenable.d.ts
+++
@@ -1,3 +0,0 @@
-export default interface Listenable<Events> {
-    readonly event: Events;
-}
